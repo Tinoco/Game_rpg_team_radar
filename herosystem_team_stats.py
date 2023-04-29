743b0daa5169317b095635b629b656719e3d7a7e
Tinoco <ticaps@ticapsoriginal.com>
2023-04-29 19:11:06 -0300

    Create herosystem_team_stats.py

---
+++ herosystem_team_stats.py
@@ -0,0 +1,28 @@
+import plotly.graph_objects as go
+import plotly.offline as pyo
+
+atributes = ['Attack', 'Agility', 'Magic Defence',
+             'Magic Attack', 'Defence']
+atributes = [*atributes, atributes[0]]
+
+paladin_1 = [35, 25, 23, 18, 23]
+mage_2 = [15, 15, 23, 23, 18]
+archer_3 = [23, 35, 15, 15, 23]
+paladin_1 = [*paladin_1, paladin_1[0]]
+mage_2 = [*mage_2, mage_2[0]]
+archer_3 = [*archer_3, archer_3[0]]
+
+party = go.Figure(
+    data=[
+        go.Scatterpolar(r=paladin_1, theta=atributes, name='Paladin 1'),
+        go.Scatterpolar(r=mage_2, theta=atributes, name='Mage 2'),
+        go.Scatterpolar(r=archer_3, theta=atributes, name='Archer 3')
+    ],
+    layout=go.Layout(
+        title=go.layout.Title(text='Rpg Game group comparison'),
+        polar={'radialaxis': {'visible': True}},
+        showlegend=True
+    )
+)
+
+pyo.plot(party)
